7bfe7dceb6760f344e2e5c72cd91f25b13eae552
Nikita Makarevich <nikita.makarevich@nokia.com>
2025-12-02 18:04:25 +0100

    work in progress

--- project1/modules.py
+++ project1/modules.py
@@ -1,21 +1,32 @@
+from collections.abc import Iterable
+from pathlib import Path
+
 import lightning as L
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from torchvision.datasets import MNIST
-from pathlib import Path
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+from torchmetrics import Accuracy
+from torchvision.datasets import MNIST
 
 
 class MnistDataModule(L.LightningDataModule):
-    def __init__(self, data_dir: str | Path, batch_size: int = 64, *, seed: int = 420):
+    def __init__(
+        self,
+        data_dir: str | Path,
+        batch_size: int = 64,
+        *,
+        num_workers: int = 0,
+        seed: int = 420,
+    ):
         super().__init__()
         self.data_dir = Path(data_dir)
         self.batch_size = batch_size
         self.transforms = transforms.Compose(
-            [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))]
+            [transforms.ToTensor(), transforms.Lambda(lambda x: x.flatten())]
         )
+        self.num_workers = num_workers
         self.seed = seed
 
     def prepare_data(self):
@@ -43,38 +54,84 @@ class MnistDataModule(L.LightningDataModule):
 
     def train_dataloader(self):
         # Return training dataloader
-        return DataLoader(self.mnist_train, batch_size=self.batch_size, shuffle=True)
+        return DataLoader(
+            self.mnist_train,
+            batch_size=self.batch_size,
+            shuffle=True,
+            num_workers=self.num_workers,
+        )
 
     def val_dataloader(self):
         # Return validation dataloader
-        return DataLoader(self.mnist_val, batch_size=self.batch_size, shuffle=False)
+        return DataLoader(
+            self.mnist_val,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
 
     def test_dataloader(self):
         # Return test dataloader
-        return DataLoader(self.mnist_test, batch_size=self.batch_size, shuffle=False)
-
-    def predict_dataloader(self):
-        return DataLoader(self.mnist_predict, batch_size=self.batch_size, shuffle=False)
-
-
-class SimplePredictor(L.LightningModule):
-    def __init__(self):
-        super().__init__()
-
-        self.mlp = nn.Sequential(
-            nn.Linear(10, 128),
-            nn.ReLU(),
-            nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Linear(64, 32),
-            nn.ReLU(),
-            nn.Linear(32, 1),
+        return DataLoader(
+            self.mnist_test,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
         )
 
-    def training_step(self, batch, batch_idx):
+    def predict_dataloader(self):
+        return DataLoader(
+            self.mnist_predict,
+            batch_size=self.batch_size,
+            shuffle=False,
+            num_workers=self.num_workers,
+        )
+
+
+class MnistClassifier(L.LightningModule):
+    def __init__(self, hidden_layer_sizes: Iterable[int], dropout: float = 0.2):
+        super().__init__()
+
+        self.save_hyperparameters(
+            {"hidden_layer_sizes": hidden_layer_sizes, "dropout": dropout}
+        )
+
+        hidden_layers = []
+        input_size = 28 * 28
+        for h in hidden_layer_sizes:
+            hidden_layers.append(nn.Linear(input_size, h))
+            hidden_layers.append(nn.Dropout(dropout))
+            hidden_layers.append(nn.ReLU())
+            input_size = h
+        self.mlp = nn.Sequential(*hidden_layers, nn.Linear(input_size, 10))
+
+        self.accuracy = Accuracy(task="multiclass", num_classes=10)
+
+    def training_step(self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx: int):
         x, y = batch
         y_hat = self.mlp(x)
-        loss = F.mse_loss(y_hat, y)
+        loss = F.cross_entropy(y_hat, y)
+        accuracy = self.accuracy(y_hat, y)
+        self.log("train/loss", loss, prog_bar=True)
+        self.log("train/accuracy", accuracy, prog_bar=True)
+        return loss
+
+    def validation_step(self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx: int):
+        x, y = batch
+        y_hat = self.mlp(x)
+        loss = F.cross_entropy(y_hat, y)
+        accuracy = self.accuracy()
+        self.log("val/loss", loss, prog_bar=True)
+        self.log("val/accuracy", accuracy, prog_bar=True)
+        return loss
+
+    def test_step(self, batch: tuple[torch.Tensor, torch.Tensor], batch_idx: int):
+        x, y = batch
+        y_hat = self.mlp(x)
+        loss = F.cross_entropy(y_hat, y)
+        accuracy = self.accuracy(y_hat, y)
+        self.log("test/loss", loss, prog_bar=True)
+        self.log("test/accuracy", accuracy, prog_bar=True)
         return loss
 
     def configure_optimizers(self):
--- project1/scripts/train.py
+++ project1/scripts/train.py
@@ -1,11 +1,21 @@
 import lightning as L
+from lightning.pytorch.loggers import WandbLogger
 
-from project1.modules import SimplePredictor
+from project1.modules import MnistClassifier, MnistDataModule
 
-
+SEED = 420
 
 if __name__ == "__main__":
-    train_loader = 
+    mnist_datamodule = MnistDataModule(
+        "./data", batch_size=64, num_workers=0, seed=SEED
+    )
+    wandb_logger = WandbLogger(
+        log_model="all", project="uni-mlops", save_dir="./wandb_logs"
+    )
 
-    model = SimplePredictor()
-    trainer = L.Trainer()
+    model = MnistClassifier(hidden_layer_sizes=[128, 64], dropout=0.2)
+
+    wandb_logger.watch(model)
+
+    trainer = L.Trainer(logger=wandb_logger, max_epochs=5)
+    trainer.fit(model, datamodule=mnist_datamodule)
